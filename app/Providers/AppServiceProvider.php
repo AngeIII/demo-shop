348fd81a2134cff084d83bec896a431798238b08
Pavel I <pavel@wpbakery.com>
2017-12-09 23:07:28 +0200

    Fix mysql issue on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Observers\Extra\BusinessOrderObserver;
 use App\Observers\OrderObserver;
 use App\Order;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,6 +17,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         Order::observe(OrderObserver::class);
 
         // Special Extra requirements
